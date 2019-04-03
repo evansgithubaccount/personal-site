cf4847bbfdfa7ef91cc7c33e174da16944b36517
evansgithubaccount <48694606+evansgithubaccount@users.noreply.github.com>
2019-04-03 07:34:43 -0400

    Add files via upload

---
+++ vinyl.js
@@ -0,0 +1,19 @@
+function growHTBAHB() {
+document.getElementById("HTBAHB").classList.toggle('growHTBAHB');
+}
+
+function growProjectors() {
+    document.getElementById("DirtyProjectors").classList.toggle('growProjectors');
+ }
+
+ function growEMS() {
+    document.getElementById("EMS").classList.toggle('growEMS');
+ }
+
+ function growDamn() {
+     document.getElementById("damn").classList.toggle('growDamn');
+ }
+
+ function growMales() {
+    document.getElementById("males").classList.toggle('growMales');
+}
